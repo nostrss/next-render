35c2931f876f3b5433de69f205fc2c0b3b7abfcf
nostrss <jintagi@gmail.com>
2023-03-05 23:25:19 +0900

    init push

---
+++ src/pages/[id]/index.jsx
@@ -0,0 +1,49 @@
+import Head from 'next/head';
+
+export default function Detail({ detailData }) {
+  return (
+    <>
+      <Head>
+        <title>{detailData.title}</title>
+        <meta name='description' content={detailData.title} />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <main>
+        {detailData && (
+          <fieldset>
+            <legend>{detailData.title}</legend>
+            <img
+              src={`https://www.artic.edu/iiif/2/${detailData.image_id}/full/400,/0/default.jpg`}
+              alt={detailData.title}
+            />
+            <li>history : {detailData.publication_history}</li>
+          </fieldset>
+        )}
+      </main>
+    </>
+  );
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { id: '14556' } },
+      { params: { id: '11434' } },
+      { params: { id: '16487' } },
+    ],
+    fallback: true,
+  };
+}
+
+export async function getStaticProps(context) {
+  const id = context.params.id;
+  const apiUrl = `https://api.artic.edu/api/v1/artworks/${id}`;
+  const res = await fetch(apiUrl);
+  const data = await res.json();
+  return {
+    props: {
+      detailData: data.data,
+    },
+  };
+}
